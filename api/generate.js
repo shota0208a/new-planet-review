87ad9b55dff8524caf34cff63085b682a6ea7f10
shota0208a <shota0208a@gmail.com>
2026-08-19 08:16:16 +0900

    generate.js を更新

--- api/generate.js
+++ api/generate.js
@@ -7,7 +7,5 @@ export default async function handler(req, res) {
   }
 
   return res.status(200).json({
-    success: true,
-    message: "API connection OK"
-  });
-}
+  review: "これはAPI接続テストです。正常に表示されています。"
+});
